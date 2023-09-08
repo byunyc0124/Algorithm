5fb29f9add84f3026ce87b4b9e96d716a5de01a3
BY <baeyonyk124@gmail.com>
2023-09-08 09:12:55 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.09 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+        for(int i=0; i<n/2; i++){
+            answer.append("수박");
+        }
+        if(n%2!=0) answer.append("수");
+        return answer.toString();
+    }
+}
